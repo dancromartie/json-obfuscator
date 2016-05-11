aee61c776fb15448271c3fcfe6a7b63abde7ddb3
Dan Cromartie <dcromartie@ondeck.com>
2016-05-11 16:17:14 -0400

    separate function for finding and searching paths

--- jsonobfuscator.py
+++ jsonobfuscator.py
@@ -5,28 +5,48 @@ import json
 import re
 import sys
 
+
 def is_scalar(val):
     return isinstance(val, (int, float, basestring))
+
+
+def get_matches(obj, path_configs):
+    all_paths = []
+    for path_config in path_configs:
+        these_paths = {"path_config": path_config, "matches": []}
+        jsonpath_expr = parse(path_config["path"])
+        for match in jsonpath_expr.find(obj):
+            match_path = str(match.full_path)
+            these_paths["matches"].append({
+                "value": match.value,
+                "path": match_path
+            })
+        all_paths.append(these_paths)
+    return all_paths
+
 
 def obfuscate(obj, path_configs):
     if not path_configs:
         sys.exit("You need at least one path config")
     clean_object = obj
     # We need to keep track of previous paths because sometimes I think it tries to replace on 
-    # its replacements, so sometimes we need to stop it.
-    for path_config in path_configs:
-        all_previous_paths = []
-        jsonpath_expr = parse(path_config["path"])
-        for match in jsonpath_expr.find(obj):
-            match_path = str(match.full_path)
+    # its replacements, so we need to stop it.
+    all_previous_paths = set()
+    matches_by_config = get_matches(obj, path_configs)
+    for mbc in matches_by_config:
+        for match in mbc["matches"]:
             for prev_path in all_previous_paths:
-                if match_path.startswith(prev_path):
+                if match["path"].startswith(prev_path):
                     sys.exit("You seem to be replacing already replaced paths..."
                              "  Maybe tone down the jsonpath?")
-            all_previous_paths.append(match_path)
-            feature_value = match.value
-            orig_obj = copy.deepcopy(obj)
-            clean_object = change_value_at_path(match_path, path_config, obj, orig_obj)
+            if match["path"] in all_previous_paths:
+                pass
+            else:
+                all_previous_paths.add(match["path"])
+                feature_value = match["value"]
+                orig_obj = copy.deepcopy(obj)
+                clean_object = change_value_at_path(
+                    match["path"], mbc["path_config"], obj, orig_obj)
     return clean_object
 
 
